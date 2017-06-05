cfd9546e5889056699425281363c84916dba0213
xushaomin <xushaomin@foxmail.com>
2017-06-05 19:38:53 +0800

    优化代码

--- apple-cim-mina/apple-cim-mina-server/src/main/assembly/bin/start.sh
+++ apple-cim-mina/apple-cim-mina-server/src/main/assembly/bin/start.sh
@@ -50,7 +50,7 @@ STDOUT_FILE=$LOGS_DIR/stdout.log
 LIB_DIR=$DEPLOY_DIR/lib
 LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
 
-JAVA_OPTS=" -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true "
+JAVA_OPTS=" -Dio.netty.noUnsafe=true -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true "
 JAVA_DEBUG_OPTS=""
 if [ "$2" = "debug" ]; then
     JAVA_DEBUG_OPTS=" -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n "
